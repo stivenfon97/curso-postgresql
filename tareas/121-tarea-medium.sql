4d7abf31ed94b143fbc1b7aab2ae9436f211598f
Stiven Fonseca <stivenfon2u@gmail.com>
2026-03-18 14:08:52 -0500

    continuo resolviendo puntos del reto

--- tareas/121-tarea-medium.sql
+++ tareas/121-tarea-medium.sql
@@ -32,7 +32,6 @@ ORDER BY cantidad_post DESC
 LIMIT 10
 
 
-
 -- 5. Quiero los 5 post con más "Claps" sumando la columna "counter"
 /*
 692	sit excepteur ex ipsum magna fugiat laborum exercitation fugiat
@@ -42,6 +41,12 @@ LIMIT 10
 502	amet exercitation tempor laborum fugiat aliquip dolore
 */
 
+SELECT p.post_id, p.title, SUM(c.counter) AS count_claps
+FROM posts p
+INNER JOIN claps c ON c.post_id = p.post_id
+GROUP BY p.post_id, p.title
+ORDER BY count_claps DESC
+LIMIT 5
 
 
 -- 6. Top 5 de personas que han dado más claps (voto único no acumulado ) *count
@@ -53,7 +58,12 @@ LIMIT 10
 6	Rose Owen
 */
 
-
+SELECT COUNT(c.counter) as voto, u.name
+FROM users u
+INNER JOIN claps c ON c.user_id = u.user_id
+GROUP BY u.name
+ORDER BY voto DESC
+LIMIT 5
 
 
 -- 7. Top 5 personas con votos acumulados (sumar counter)
@@ -65,14 +75,22 @@ LIMIT 10
 364	Lillian Hodge
 */
 
-
+SELECT SUM(c.counter) as counter_votos, u.name
+FROM users u
+INNER JOIN claps c ON c.user_id = u.user_id
+GROUP BY u.name
+ORDER BY counter_votos DESC
+LIMIT 5
 
 -- 8. Cuantos usuarios NO tienen listas de favoritos creada
 -- 329
 
+SELECT COUNT(u.user_id) as COUNT
+FROM users u
+LEFT JOIN user_lists ul ON ul.user_id = u.user_id
+WHERE ul.user_list_id IS NULL
 
-
--- 9. Quiero el comentario con id #1
+-- 9. Quiero el comentario con id
 -- Y en el mismo resultado, quiero sus respuestas (visibles e invisibles)
 -- Tip: union
 /*
@@ -102,9 +120,5 @@ LIMIT 10
 -- ** 11. Avanzado
 -- Listar todos los comentarios principales (no respuestas) 
 -- Y crear una columna adicional "replies" con las respuestas en formato JSON
-
-
-
-
 
 
